f617db3652cbac26eebcb9bbd0018ec9657c534a
Oleg Fostenko <oleg.fostenko.03@mail.ru>
2024-06-23 14:32:39 +0300

    Add files via upload

---
+++ Week 9 (Flask)/Problem Set 9/birthdays/app.py
@@ -0,0 +1,45 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, flash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        req = request.form
+        if req['name'] and req['month'] and req['day']:
+            db.execute('INSERT INTO birthdays (name, month, day) VALUES ((?), (?), (?))', req['name'], req['month'], req['day'])
+            return redirect('/')
+        else:
+            return redirect('/')
+    else:
+        birthdays = db.execute('SELECT * FROM birthdays')
+        return render_template("index.html", birthdays=birthdays)
+
+
+@app.route('/delete', methods=['POST'])
+def delete_birthday():
+    birthday_id = request.form.get('id')
+    db.execute('DELETE FROM birthdays WHERE id = (?)', birthday_id)
+    return redirect('/')
+
+
